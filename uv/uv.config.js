bd3ed4f9f2d2f5bea00936b981c1b813921bc129
posh <101807149+ImPosh@users.noreply.github.com>
2024-01-05 22:06:00 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://edu.primemath.lol/bare/',
+    bare: 'https://savethetrees.sen.org.nz/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
